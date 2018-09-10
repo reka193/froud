3c6795bcbf22b408702ec312e356d147c944b92e
reka193 <reka193@hotmail.com>
2018-09-10 10:41:05 +0200

    Adding command line arguments

--- rolepolicies.py
+++ rolepolicies.py
@@ -3,10 +3,19 @@ import boto3
 import requests
 import json
 from prettytable import PrettyTable
+import argparse
 
 import logging
 from logging.handlers import SysLogHandler
 from logging import Formatter
+import re
+
+
+
+parser = argparse.ArgumentParser(description=' !!! DESCRIPTION GOES HERE !!! ')
+parser.add_argument('-rft','--regex-filter-type', help='Select the column you wish to filter. E.g. "Service", "Action", "Resource", "Effect" or "Policy name".', required=True)
+parser.add_argument('-rf','--regex-filter', help='Display only those entities that match the entered regular expression.', required=True)
+args = vars(parser.parse_args())
 
 # Syslog handler
 syslog = SysLogHandler(address='/dev/log')
@@ -54,7 +63,6 @@ def policy_enumerate():
         for pol_stat in policy_statement:
             resource = pol_stat['Resource']
             effect = pol_stat['Effect']
-
             for action in pol_stat['Action']:
                 values2.append([action.split(':')[0], action.split(':')[1], resource, effect, role_policy2.name])
 
@@ -69,16 +77,58 @@ def policy_enumerate():
     x.align["Policy name"] = "l"
 
     for value in values:
-        x.add_row(value)
+        #print (str(value)+"\n")
+        value_parsed_json = ""
+        value_parsed_json = json.load(value)
+        if "Service" == str(args['regex-filter-type']):
+            if re.match(args['regex-filter'], value_parsed_json[0]):
+                x.add_row(value)
+        # Repeat the above logic for each column
+        #...
+        #...
+                #print(x)
 
-    print(x)
+
+# def try_resources():
+#
+#     lambda_client = boto3.client('lambda', region_name='us-west-2')
+#
+#     try:
+#         print('\nPrinting the functions in Lambda:')
+#         print(lambda_client.list_functions())
+#     except Exception as e:
+#         print('Error: {}'.format(e))
+#
+#     role_arn_mod = ':'.join(role_arn.split(':')[:5]) + ':role/' + role_arn.split('/')[1]
+#
+#     with open('lambda.zip', 'rb') as f:
+#       zipped_code = f.read()
+#
+#     try:
+#         print('\nCreating a new function in Lambda:')
+#         lambda_client.create_function(
+#           FunctionName='myLambdaFunction',
+#           Runtime='python2.7',
+#           Role=role_arn_mod,
+#           Handler='main.handler',
+#           Code=dict(ZipFile=zipped_code)
+#         )
+#     except Exception as e:
+#         print('Error: {}'.format(e))
+#
+#     ec2 = boto3.resource('ec2', region_name='us-west-2')
+#     ami_ids = []
+#     for instance in ec2.instances.all():
+#         ami_ids.append(instance.image_id)
+#     try:
+#         ec2.create_instances(ImageId=ami_ids[1], MinCount=1, MaxCount=5)
+#     except Exception as e:
+#         print('Error: {}'.format(e))
 
 if __name__ == '__main__':
     arn = ARN()
     # services = arn.service.choices()
-    services = ['s3', 'dynamodb', 'sqs']
-    # services = ['s3']
-    services.sort()
+
 
     iam = boto3.client('iam')
     iamres = boto3.resource('iam')
@@ -87,3 +137,4 @@ if __name__ == '__main__':
     role = role_arn.split('/')[1]
 
     policy_enumerate()
+    # try_resources()
