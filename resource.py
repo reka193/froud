c3f0caf512bcf9daa2a6cce66e619994bdb6795d
reka193 <reka193@hotmail.com>
2018-09-13 09:59:35 +0200

    Small fixes

--- resource.py
+++ resource.py
@@ -2,6 +2,7 @@ import skew
 from skew.arn import ARN
 import boto3
 from prettytable import PrettyTable
+import json
 
 import logging
 from logging.handlers import SysLogHandler
@@ -16,37 +17,66 @@ logger = logging.getLogger()
 logger.setLevel(logging.DEBUG)
 logger.addHandler(syslog)
 
-arn = ARN()
-services = ['s3', 'dynamodb', 'sqs']
-services.sort()
 
-print('Enumerating all resources in the following services: ' + ', '.join(services) + '\n')
+def enum_resources(services):
+    print('Enumerating all resources in the following services: ' + ', '.join(services) + '\n')
 
-values = []
-for service in services:
-    arn.service.pattern = service
-    try:
-        instances = skew.scan('{}/*'.format(arn))
-        print('arn: {}'.format(arn))
-        if instances:
-            for instance in instances:
-                print(instance)
-                region = str(instance).split(':')[3]
-                queue_name = str(instance).split('/')[1]
-                values.append([service, region, queue_name])
+    values = []
+    values_pol = []
+    for service in services:
+        arn.service.pattern = service
+        try:
+            instances = skew.scan('{}/*'.format(arn))
+            if instances:
+                for instance in instances:
+                    region = str(instance).split(':')[3]
+                    resource_name = str(instance).split('/')[1]
+                    values.append([service, region, resource_name])
 
-    except Exception as e:
-        print(e)
+                    if service == 's3':
+                        s3 = boto3.resource('s3')
+                        bucket = s3.Bucket(resource_name)
+                        try:
+                            pol = json.loads(s3.BucketPolicy(bucket.name).policy)
+                            try:
+                                sid = pol['Statement'][0]['Sid']
+                            except:
+                                sid = ''
+                            action = pol['Statement'][0]['Action']
+                            principal = pol['Statement'][0]['Principal']['AWS']
+                            resource = pol['Statement'][0]['Resource']
+                            effect = pol['Statement'][0]['Effect']
 
-values.sort()
-x = PrettyTable()
-x.field_names = ["Service", "Region", "Name"]
-x.align["Service"] = "l"
-x.align["Region"] = "l"
-x.align["Name"] = "l"
+                            values_pol.append([resource_name, sid, action, principal, resource, effect])
+                        except:
+                            pass
+        except Exception as e:
+            print(e)
+    return values, values_pol
 
-for value in values:
-    x.add_row(value)
 
-print('\nAvailable resources: \n')
-print(x)
+def print_table(values, fieldnames):
+    values.sort()
+    x = PrettyTable()
+    x.field_names = fieldnames
+    for field in fieldnames:
+        x.align[field] = "l"
+
+    for value in values:
+        x.add_row(value)
+
+    print(x)
+
+
+if __name__ == '__main__':
+    arn = ARN()
+    # services = ['s3', 'dynamodb', 'sqs']
+    services = ['sqs']
+    services.sort()
+    values, values_pol = enum_resources(services)
+
+    print('\nAvailable resources: \n')
+    print_table(values, ["Service", "Region", "Name"])
+    if 's3' in services:
+        print('\nAttached S3 policies: \n')
+        print_table(values_pol, ["Name", "Sid", "Action", "Principal", "Resource", "Effect"])
