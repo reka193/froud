3e4fe0759e0d6d37f6d4b63fc6321934b011b1dc
reka193 <reka193@hotmail.com>
2018-10-05 17:26:53 +0200

    Update cloudwatch.py

--- cloudwatch.py
+++ cloudwatch.py
@@ -155,7 +155,7 @@ def list_and_save(logs_client, args):
 
 def upload_files(s3_client, filenames, bucket_name):
 
-    print('Uploading files...')
+    print('Uploading files to the bucket {}...'.format(bucket_name))
     for f in filenames:
         try:
             key = f.split('/')[-2:]
@@ -165,7 +165,7 @@ def upload_files(s3_client, filenames, bucket_name):
             t.upload_file(f, bucket_name, key, extra_args={'ACL': 'public-read'})
 
             file_url = 'https://{}.s3.amazonaws.com/{}'.format(bucket_name, key)
-            print('The uploaded file is public and accessible with the following url: {}'.format(file_url))
+            print('The uploaded file is public and accessible with the following url: \n    {}'.format(file_url))
         except S3UploadFailedError:
             print('File upload is not successful: PutObject permission missing.')
 
