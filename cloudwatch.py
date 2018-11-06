8bb85cadbd329ad8b8c1f71773b11fcb03261ea4
reka193 <reka193@hotmail.com>
2018-11-06 16:29:48 +0100

    common.py

--- cloudwatch.py
+++ cloudwatch.py
@@ -8,7 +8,7 @@ import argparse
 import requests
 from argparse import RawTextHelpFormatter
 from prettytable import PrettyTable
-from boto3.exceptions import S3UploadFailedError
+from common import upload_files
 
 
 def init():
@@ -151,23 +151,6 @@ def list_and_save(logs_client, args):
 
     except Exception as e:
             print(e)
-
-
-def upload_files(s3_client, filenames, bucket_name):
-
-    print('Uploading files to the bucket {}...'.format(bucket_name))
-    for f in filenames:
-        try:
-            key = f.split('/')[-2:]
-            key = key[0] + '/' + key[1]
-            tc = boto3.s3.transfer.TransferConfig()
-            t = boto3.s3.transfer.S3Transfer(client=s3_client, config=tc)
-            t.upload_file(f, bucket_name, key, extra_args={'ACL': 'public-read'})
-
-            file_url = 'https://{}.s3.amazonaws.com/{}'.format(bucket_name, key)
-            print('The uploaded file is public and accessible with the following url: \n    {}'.format(file_url))
-        except S3UploadFailedError:
-            print('File upload is not successful: PutObject permission missing.')
 
 
 def print_table(values):
