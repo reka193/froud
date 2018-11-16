301253c2dcc4c96ba5a0e1cba75b2c1c28633c2c
reka193 <reka193@hotmail.com>
2018-11-16 15:19:16 +0100

    Update sqs.py

--- sqs.py
+++ sqs.py
@@ -1,10 +1,10 @@
-import boto3
-import sys
+from botocore.exceptions import ClientError
 import common
+import sys
 
 
 def init():
-    #init_keys()
+
     description = '\n[*] SQS message scanner.\n' \
                   '[*] Specify the name of the queue to save the messages from.\n' \
                   '[*] If a bucket is provided, the results are uploaded to the bucket. \n\n'
@@ -14,26 +14,20 @@ def init():
 
     args = common.parsing(description, required_params, optional_params)
 
-    # If the config file cannot be loaded, boto3 will use the credentials from ~/.aws/credentials
-    config_parsing_was_successful, region_name_for_logs = common.load_config_json("conf.json")
+    config_success, data = common.load_config_json("conf.json")
 
-    if not config_parsing_was_successful:
-        region_name_for_logs = "N/A"
+    # If the config file can not be found, shared credentials are used from ~/.aws/credentials and /config
+    sqs_client, s3_client = common.create_client(config_success, data, 'sqs')
 
-    session = boto3.Session()
-    s3_client = session.client('s3')
-
-    return args, region_name_for_logs, s3_client
+    return args, sqs_client, s3_client
 
 
-def scan_queue(queue_name):
-    sqs = boto3.resource('sqs', region_name='local', aws_access_key_id='asd', aws_secret_access_key='asd',
-                         endpoint_url='http://localhost:8001')
-    #sqs = boto3.resource('sqs', region_name='eu-west-1')
-    #queue_name = 'mrupdater-notifs'
-    print('queuename: {}'.format(queue_name))
-
-    queue = sqs.create_queue(QueueName=queue_name)
+def scan_queue(queue_name, sqs):
+    try:
+        queue = sqs.create_queue(QueueName=queue_name)
+    except ClientError as err:
+        print(err)
+        sys.exit()
 
     # get messages
     msgs = []
@@ -49,14 +43,15 @@ def scan_queue(queue_name):
 
 def main():
 
-    args, region_name_for_logs, s3_client = init()
+    args, sqs, s3_client = init()
 
     queue_name = str(args['queueName'])
-    
-    data = scan_queue(queue_name)
-    filenames = common.write_to_file('sqs', queue_name, data)
 
-    common.bucket_upload(args['bucket'], s3_client, filenames)
+    data = scan_queue(queue_name, sqs)
+    filenames = common.write_to_file_1000('sqs', queue_name, data)
+
+    if args['bucketName']:
+        common.bucket_upload(args['bucket'], s3_client, filenames)
 
 
 if __name__ == '__main__':
